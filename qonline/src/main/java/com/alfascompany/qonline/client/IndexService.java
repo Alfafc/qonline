5af52ab072b80232c6f4426ed380bbe1d9f0588d
Fernando Costantini <fhcostantini@gmail.com>
2014-01-31 00:53:09 -0300

    Init repository

---
+++ qonline/src/main/java/com/alfascompany/qonline/client/IndexService.java
@@ -0,0 +1,13 @@
+package com.alfascompany.qonline.client;
+
+public class IndexService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ qonline/src/main/java/com/alfascompany/qonline/server/IndexServerImpl.java
@@ -0,0 +1,13 @@
+package com.alfascompany.qonline.server;
+
+public class IndexServerImpl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
